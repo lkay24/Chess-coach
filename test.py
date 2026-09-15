a91fe359be5ab1a297e6b66892d85c6ebe7027f9
lkay24 <12alkshaybazaz@gmail.com>
2026-09-15 06:48:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import chess
 import chess.engine
 
-# Set up a starting chess board
 board = chess.Board()
 
-# Connect to Stockfish
 engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 
-# Ask Stockfish: what's the best move here?
 result = engine.play(board, chess.engine.Limit(time=0.5))
 
 print("Stockfish says the best move is:", result.move)
